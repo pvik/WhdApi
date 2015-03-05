661a1df69f93cd1893790fb13e3e728dafe682b2
PRAVEEN VIKRAM <praveen@kiosa.org>
2015-03-04 20:37:38 -0500

    test cases written in AuthUnitTest

--- src/test/java/com/whd/test/TestDetailsSample.java
+++ src/test/java/com/whd/test/TestDetailsSample.java
@@ -4,4 +4,5 @@ public class TestDetailsSample {
     static final String whdUri = "";
     static final String username = "";
     static final String password = "";
+    static final String apiKey = "";
 }
