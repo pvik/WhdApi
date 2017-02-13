a9cdc1468ab1c2af796a8a2b7ec5b184974b8e54
Praveen Vikram <praveen.vikram@kiosa.org>
2017-02-12 20:28:25 -0500

    start WhdServerObjects development with WhdLocations

--- src/main/java/com/whd/WhdServerObjects.java
+++ src/main/java/com/whd/WhdServerObjects.java
@@ -1,7 +1,19 @@
 package com.whd;
 
+import com.whd.so.WhdLocations;
+
 /**
  * Created by vikramp on 2/12/17.
  */
 public class WhdServerObjects {
+
+    private final WhdLocations locations ;
+
+    public WhdServerObjects() throws WhdException {
+        throw new WhdException("Invalid invocation of WhdServerObjects");
+    }
+
+    public WhdServerObjects(WhdAuth auth) throws WhdException {
+        this.locations = new WhdLocations(WhdApi.getLocationList(auth));
+    }
 }
--- src/main/java/com/whd/so/WhdLocations.java
+++ src/main/java/com/whd/so/WhdLocations.java
@@ -1,7 +1,37 @@
 package com.whd.so;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+import com.whd.autogen.LocationDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 /**
  * Created by vikramp on 2/12/17.
  */
 public class WhdLocations {
+
+    private static final Logger logger = LoggerFactory.getLogger(WhdLocations.class);
+
+    private final BiMap<Integer, String> locationMap;
+
+    public WhdLocations(List<LocationDefinition> locationDefs) {
+        ImmutableBiMap.Builder<Integer, String> mapBuilder = new ImmutableBiMap.Builder<>();
+
+        for(LocationDefinition loc : locationDefs) {
+            mapBuilder.put(loc.getId(), loc.getLocationName());
+        }
+
+        locationMap = mapBuilder.build();
+    }
+
+    public String getLocationName(Integer id) {
+        return locationMap.get(id);
+    }
+
+    public Integer getLocationId(String locName) {
+        return locationMap.inverse().get(locName);
+    }
 }
