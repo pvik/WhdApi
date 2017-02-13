c8b42ad58d7ee6b4ad10eaadc0160f1722e1230b
Praveen Vikram <praveen.vikram@kiosa.org>
2017-02-12 21:28:15 -0500

    adding WhdPriority server object

--- src/main/java/com/whd/WhdServerObjects.java
+++ src/main/java/com/whd/WhdServerObjects.java
@@ -1,19 +1,41 @@
 package com.whd;
 
 import com.whd.so.WhdLocations;
+import com.whd.so.WhdPriority;
+
+import java.util.List;
 
 /**
  * Created by vikramp on 2/12/17.
  */
 public class WhdServerObjects {
 
-    private final WhdLocations locations ;
+    private final WhdLocations locations;
+    private final WhdPriority whdPriority;
 
     public WhdServerObjects() throws WhdException {
         throw new WhdException("Invalid invocation of WhdServerObjects");
     }
 
-    public WhdServerObjects(WhdAuth auth) throws WhdException {
-        this.locations = new WhdLocations(WhdApi.getLocationList(auth));
+    public WhdServerObjects(WhdAuth auth)
+            throws WhdException {
+        this.locations = new WhdLocations(auth);
+
+        this.whdPriority = new WhdPriority(auth);
+    }
+
+    public WhdServerObjects(WhdAuth auth, List<String> locationPrefixes)
+            throws WhdException {
+        this.locations = new WhdLocations(auth, locationPrefixes);
+
+        this.whdPriority = new WhdPriority(auth);
+    }
+
+    public WhdLocations getLocations() {
+        return locations;
+    }
+
+    public WhdPriority getPriority() {
+        return whdPriority;
     }
 }
--- src/main/java/com/whd/so/WhdPriority.java
+++ src/main/java/com/whd/so/WhdPriority.java
@@ -1,7 +1,42 @@
 package com.whd.so;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+import com.whd.WhdApi;
+import com.whd.WhdAuth;
+import com.whd.WhdException;
+import com.whd.autogen.PriorityTypeDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by vikramp on 2/12/17.
  */
 public class WhdPriority {
+
+    private static final Logger logger = LoggerFactory.getLogger(WhdLocations.class);
+
+    private final BiMap<Integer, String> priorityMap;
+
+    public WhdPriority(WhdAuth auth) throws WhdException {
+        ImmutableBiMap.Builder<Integer, String> mapBuilder = new ImmutableBiMap.Builder<>();
+
+        for(PriorityTypeDefinition p : WhdApi.getPriorityTypeDefinitions(auth)) {
+            mapBuilder.put(p.getId(), p.getPriorityTypeName());
+        }
+
+        priorityMap = mapBuilder.build();
+    }
+
+    public String getPriorityName(Integer id) {
+        return priorityMap.get(id);
+    }
+
+    public Integer getPriorityId(String locName) {
+        return priorityMap.inverse().get(locName);
+    }
+
+    public BiMap<Integer, String> getPriorityMap() {
+        return priorityMap;
+    }
 }
