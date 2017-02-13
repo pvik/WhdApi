95f055dc8f8c276d144199d9417f447494f005e2
Praveen Vikram <praveen.vikram@kiosa.org>
2017-02-12 20:16:22 -0500

    adding WhdServerObjects and WhdLocations

---
+++ src/main/java/com/whd/WhdServerObjects.java
@@ -0,0 +1,7 @@
+package com.whd;
+
+/**
+ * Created by vikramp on 2/12/17.
+ */
+public class WhdServerObjects {
+}
---
+++ src/main/java/com/whd/so/WhdLocations.java
@@ -0,0 +1,7 @@
+package com.whd.so;
+
+/**
+ * Created by vikramp on 2/12/17.
+ */
+public class WhdLocations {
+}
