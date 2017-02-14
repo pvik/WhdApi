704f6665c4147abab2198fdd66e90fc3d740e4ff
Praveen Vikram <praveen.vikram@kiosa.org>
2017-02-13 20:18:41 -0500

    adding get and set customField methods

--- src/main/java/com/whd/serverobject/WhdCustomFields.java
+++ src/main/java/com/whd/serverobject/WhdCustomFields.java
@@ -6,14 +6,21 @@ import com.whd.WhdApi;
 import com.whd.WhdAuth;
 import com.whd.WhdException;
 import com.whd.autogen.CustomFieldDefinition;
+import com.whd.autogen.ticket.TicketCustomField;
+import com.whd.autogen.ticket.WhdTicket;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  * Created by vikramp on 2/13/17.
  */
 public class WhdCustomFields {
-    private static final Logger logger = LoggerFactory.getLogger(WhdCustomFields.class);
+    private static final Logger log = LoggerFactory.getLogger(WhdCustomFields.class);
 
     private final BiMap<Integer, String> customFieldMap;
 
@@ -25,6 +32,62 @@ public class WhdCustomFields {
         }
 
         customFieldMap = mapBuilder.build();
+    }
+
+    public String getCustomField(WhdTicket ticket, String customFieldName) throws WhdException {
+        log.debug("Getting Custom field for field name: {}", customFieldName);
+
+        try {
+            Integer custFieldId;
+            if (customFieldMap.inverse().containsKey(customFieldName.replace(" ", "")))
+                custFieldId = customFieldMap.inverse().get(customFieldName.replace(" ", ""));
+            else
+                throw new WhdException("Custom Field not present in WHD instance");
+
+            log.debug("Field ID: {}", custFieldId);
+
+            // First check the List<TicketCustomField> for custom field
+            for(TicketCustomField cf : ticket.getTicketCustomFields()){
+                if (cf.getDefinitionId() == custFieldId) {
+                    return cf.getRestValue();
+                }
+            }
+
+            Method getCustFieldMethod = WhdTicket.class.getMethod("getCustomField" + custFieldId);
+
+            return (String) getCustFieldMethod.invoke(ticket);
+
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            log.error(ExceptionUtils.getStackTrace(ex));
+            throw new WhdException(ex);
+        }
+    }
+
+    public void setCustomField(WhdTicket ticket, String customFieldName, String fieldVal) throws WhdException {
+        log.debug("Getting Custom field for field name: {}", customFieldName);
+        try {
+            Integer custFieldId;
+            if (customFieldMap.inverse().containsKey(customFieldName.replace(" ", "")))
+                custFieldId = customFieldMap.inverse().get(customFieldName.replace(" ", ""));
+            else
+                throw new WhdException("Custom Field not present in WHD instance");
+
+            log.debug("Field ID: {}", custFieldId);
+
+            if (ticket.getTicketCustomFields() == null) {
+                ticket.setTicketCustomFields(new ArrayList<>());
+            }
+
+            TicketCustomField custField = new TicketCustomField();
+            custField.setDefinitionId(custFieldId);
+            custField.setRestValue(fieldVal);
+
+            ticket.getTicketCustomFields().add(custField);
+
+        } catch ( SecurityException | IllegalArgumentException ex) {
+            log.error(ExceptionUtils.getStackTrace(ex));
+            throw new WhdException(ex);
+        }
     }
 
     public String getCustomFieldName(Integer id) {
